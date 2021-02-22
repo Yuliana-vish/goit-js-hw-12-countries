d516bf435ed6cd1528653cb84dbed10d3b8b6e87
Yuliana-vish <68988546+Yuliana-vish@users.noreply.github.com>
2021-02-22 18:35:18 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import './styles.css';
+import fetchCountries from './js/fetchCountries';
+import refs from './js/refs';
+import debounce from 'lodash.debounce';
+import countriesList from './template/countriesList.hbs';
+import countryInfo from './template/countryInfo.hbs';
+
+import { error } from '@pnotify/core/';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+
+const dataShow = countries => {
+  if (countries.length > 10) {
+    error({
+      text: 'Too many matches found. Please enter a more specific query!',
+      delay: 2000,
+    });
+  }
+  if (countries.length > 1 && countries.length <= 10) {
+    refs.content.innerHTML = countriesList(countries);
+  }
+  if (countries.length === 1) {
+    refs.content.innerHTML = countryInfo(...countries);
+  }
+};
+
+const searchCountry = () => {
+  if (refs.search.value.length > 0) {
+    refs.content.innerHTML = '';
+    fetchCountries(refs.search.value).then(response => dataShow(response));
+  }
+};
+
+refs.search.addEventListener('input', debounce(searchCountry, 500));
